29b6758859e9bcece5fc17df0b11e0975a671276
Clémence CHOMEL <clemence.chomel@gmail.com>
2026-04-12 22:34:40 +0200

    Adding countdown

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use DateTimeImmutable;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,7 +15,23 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
-        return $this->render('home/index.html.twig');
+        $wedding = (new DateTimeImmutable('2028-09-09', new DateTimeZone('Europe/Paris')))->setTime(0, 0, 0);
+        $now = new DateTimeImmutable('now', new DateTimeZone('Europe/Paris'));
+        $diffSeconds = $wedding->getTimestamp() - $now->getTimestamp();
+        $ended = $diffSeconds <= 0;
+
+        $countdown = [
+            'ended' => $ended,
+            'endIso' => $wedding->format('c'),
+            'days' => $ended ? 0 : intdiv($diffSeconds, 86400),
+            'hours' => $ended ? '00' : sprintf('%02d', intdiv($diffSeconds % 86400, 3600)),
+            'minutes' => $ended ? '00' : sprintf('%02d', intdiv($diffSeconds % 3600, 60)),
+            'seconds' => $ended ? '00' : sprintf('%02d', $diffSeconds % 60),
+        ];
+
+        return $this->render('home/index.html.twig', [
+            'countdown' => $countdown,
+        ]);
     }
 
     #[Route('/informations', name: 'app_informations')]
